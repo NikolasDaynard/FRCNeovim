11219fd5fcfdf237e254bdaba9cbdb1d375a81d7
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-04 09:29:55 -0500

    java home bug fix

--- lua/FRCNeovim.lua
+++ lua/FRCNeovim.lua
@@ -37,10 +37,9 @@ function M.deployRobotCode()
     'cd ' .. M.robot_directory .. ' && ./gradlew deploy -PteamNumber=' .. M.teamNumber .. ' --offline',
   }
   if M.javaHome ~= '' then
-    table.insert(predefined_commands, ' -Dorg.gradle.java.home="' .. M.javaHome .. '"')
+    predefined_commands = predefined_commands .. ' -Dorg.gradle.java.home="' .. M.javaHome .. '"'
   end
-  -- Convert the table to a string
-  predefined_commands = table.concat(predefined_commands, '')
+  
   M.runCommands(predefined_commands, vim.fn.getcwd(), vim.fn.expand('%:p')) -- expand('%:p') returns the full path of the current file
 end
 
@@ -49,10 +48,8 @@ function M.buildRobotCode()
     'cd ' .. M.robot_directory .. ' && ./gradlew build',
   }
   if M.javaHome ~= '' then
-    table.insert(predefined_commands, ' -Dorg.gradle.java.home="' .. M.javaHome .. '"')
+    predefined_commands = predefined_commands .. ' -Dorg.gradle.java.home="' .. M.javaHome .. '"'
   end
-  -- Convert the table to a string
-  predefined_commands = table.concat(predefined_commands, '')
 
   M.runCommands(predefined_commands, vim.fn.getcwd(), vim.fn.expand('%:p')) -- expand('%:p') returns the full path of the current file
 end
