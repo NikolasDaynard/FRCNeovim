ede4901d78ce97a4eb29253b8c9eefbf3d857fef
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-06 11:27:45 -0500

    reuse terminal

--- lua/FRCNeovim.lua
+++ lua/FRCNeovim.lua
@@ -162,9 +162,9 @@ function openTerminal()
   end
   
   if M.terminal_size < width / 2 then -- normal case
-    vim.cmd('vsplit | vertical resize ' .. M.terminal_size)
+    vim.cmd('vsplit | vertical resize ' .. M.terminal_size .. ' | terminal')
   else -- terminal_size is greater than half of the window width so open at half
-    vim.cmd('vsplit')
+    vim.cmd('vsplit | terminal')
   end
 end
 function closeTerminal(command)
