602c2824c7dfb667c1d6508f1d0baddffb23ba3a
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-06 22:57:13 -0500

    debug prints

--- lua/utils.lua
+++ lua/utils.lua
@@ -50,7 +50,7 @@ end
 function getBufferDirectory(buf)
   -- Get the full path of the buffer's file
   local buffer_path = vim.fn.expand(vim.api.nvim_buf_get_name(buf))
-  print(buffer_path.lower() .. ' & ' .. vim.fn.expand('~/swerve2024/').lower())
+  print(lower(buffer_path) .. ' & ' .. lower(vim.fn.expand('~/swerve2024/')))
   return buffer_path
 end
 
